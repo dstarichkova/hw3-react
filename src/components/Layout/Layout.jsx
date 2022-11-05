613fbc19a0f765c1ce6043831135cb2be89ba746
daria <dstarichkova@gmail.com>
2022-11-05 16:04:05 +0300

    cart icon and button fixes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,7 +3,10 @@ import styles from "../Layout/styles.module.css";
 
 export const Layout = (props, className) => {
     return <div>
-        <header className={classnames(styles.header, className)}>Магазин</header>
+        <header className={classnames(styles.header, className)}>
+            <div>Магазин</div>
+            <div className={classnames(styles.cart__icon, className)}/>
+        </header>
         <div className={classnames(styles.main, className)}>
             { props.children }
         </div>
